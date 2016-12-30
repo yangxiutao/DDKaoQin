66b27f052fd2765ea31e184589947d7ba4432187
Yxt <2548364800@qq.com>
2016-12-30 09:53:29 +0800

    Create new project DD

---
+++ DD/DD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DD
+//
+//  Created by YXT on 2016/12/30.
+//  Copyright © 2016年 YXT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
